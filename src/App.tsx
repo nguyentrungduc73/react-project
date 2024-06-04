c5f891b2dca615c5e302e7d25c33b75326a7a67a
duc <nguyentrungduc7394@gmail.com>
2024-06-04 14:58:33 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Input from './components/Input'
+import Home from './pages/Home'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,9 +11,11 @@ function App() {
   return (
     <>
       <div>
+        <Home />
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
+        <Input />
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import Input from "src/components/Input"
+
+const Home = () => {
+  return (
+    <>
+        <Input />
+    </>
+  )
+}
+
+export default Home
